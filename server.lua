34946a6a27c2dfb11d57d15917ac532f28d36c03
Bjorn Andersson <bjorn@iki.fi>
2018-12-20 23:36:24 +0200

    Refactoring and more features, JSON command interface

--- server.lua
+++ server.lua
@@ -23,6 +23,13 @@ local CLASS_SNGBTN = 3
 local CLASS_LEV = 4
 local CLASS_MOVABLE_LEV = 5
 
+local CMD_BTN = 1
+local CMD_CMD = 2
+local CMD_SUB = 3
+
+local BTN_UP = 0
+local BTN_DOWN = 1
+local BTN_SET = 2
 
 function LuaExportStart()
 	logf = io.open(lfs.writedir().."/Logs/dcs-master.log", "w")
@@ -48,9 +55,9 @@ function LuaExportStart()
 end
 
 function LuaExportStop()
-	if client then
+	for _, client in pairs(clients) do
 		client:close()
-		client = nil
+		clients[client] = nil
 	end
 	logmsg("DCS Master stopped")
 end
@@ -64,30 +71,32 @@ function LuaExportBeforeNextFrame()
 			logmsg("Received: "..data)
 			-- FIX: split on newline
 			-- FIX: check length
-			local cmd = data:sub(1, 1):lower()
-			if cmd == 'b' then
-				handle_button(client, data)
+			local cmd = JSON:decode(data)
+			if #cmd == 0 then
+				logmsg("Data contains no command")
 				return
 			end
-			if cmd == 's' then
-				handle_subscribe(client, data)
+			local arg1 = cmd[1]
+			if type(arg1) ~= "number" then
+				logmsg("Command must be number")
 				return
 			end
-			logmsg(string.format("Unknown command '%s', cmd"))
+			if arg1 == CMD_BTN then
+				handle_button(client, cmd)
+				return
+			end
+			if arg1 == CMD_SUB then
+				handle_subscribe(client, cmd)
+				return
+			end
+			logmsg(string.format("Unknown command '%s'", arg1))
 			return
-			--reply = assert(loadstring("return "..data))()
-			--local s = string.format("%s\n", reply)
-			--send_to_client(s)
-			--GetDevice(31):performClickableAction(3008, 0.025)
-			--GetDevice(31):SetCommand(3008, 0.025)
---			gauges:set_argument_value(191, 0.0)
---			gauges:update_arguments()
 		end
 	end
 end
 
---function LuaExportAfterNextFrame()
-function LuaExportActivityNextEvent(t)
+function LuaExportAfterNextFrame()
+--function LuaExportActivityNextEvent(t)
 	if not ready then return end
 
 	local client_socket = server:accept()
@@ -102,7 +111,7 @@ function LuaExportActivityNextEvent(t)
 	for k, client in pairs(clients) do
 		send_data(client)
 	end
-	return t + 0.1
+--	return t + 0.1
 end
 
 function dump(t, i)
@@ -131,17 +140,23 @@ function string_split(s)
 end
 
 function logmsg(msg)
-	logf:write(string.format("%5.3f %s\r\n", LoGetModelTime(), msg))
+	logf:write(string.format("%8.3f %s\r\n", LoGetModelTime(), msg))
 end
 
 function interpolate(x, xx, yy)
 	-- FIX: handle reverse order
 	if x == nil then return nil end
+
 	local i
-	for j, v in ipairs(xx) do
-		if x <= v then
-			i = j
-			break
+
+	if #xx == 2 then
+		i = 2
+	else
+		for j, v in ipairs(xx) do
+			if x <= v then
+				i = j
+				break
+			end
 		end
 	end
 	return yy[i-1] + (x - xx[i-1])*((yy[i] -yy[i-1]) / (xx[i] - xx[i-1]))
@@ -159,13 +174,12 @@ end
 
 function load_gauges(aircraft)
 	local temp_gauges = read_json(lfs.writedir().."/Scripts/dcs-master/aircraft/"..aircraft.."/gauges.json")
-	if temp_gauges == nil then
+	if not temp_gauges then
 		logmsg("Failed to read gauges")
 		return false
 	end
 	gauge_defs = {}
 	for k, v in pairs(temp_gauges) do
---		logmsg(k)
 		v.name = k
 		gauge_defs[k:lower()] = v
 		if v.arg_number then
@@ -175,45 +189,97 @@ function load_gauges(aircraft)
 	return true
 end
 
-function read_cockpit(aircraft)
-	clickables = read_json(lfs.writedir().."/Scripts/dcs-master/aircraft/"..aircraft.."/clickables.json")
-	if clickables == nil then
+
+function load_clickables(aircraft)
+	local temp_clickables = read_json(lfs.writedir().."/Scripts/dcs-master/aircraft/"..aircraft.."/clickables.json")
+	if not temp_clickables then
 		logmsg("Failed to read clickables")
 		return false
+	end
+	clickables = {}
+	for k, v in pairs(temp_clickables) do
+		clickables[k:lower()] = v
+	end
+	return true
+end
+
+function read_cockpit(aircraft)
+	if not load_clickables(aircraft) then
+		return
 	end
 	return load_gauges(aircraft) 
 end
 
+-- action: BTN_UP, BTN_DOWN, BTN_SET
+-- clickable: Name of clickable
+-- value: Must be set if action == BTN_SET
+function scroll_clickable(action, clickable, value)
 
-
-
-function click_clickable(pos, action, clickable)
-
-	if pos ~= 1 and pos ~= 2 then
-		logmsg("Only butten 1 or 2 allowed, "..pos.." given")
-		return
-	end
-	
 	local i = clickables[clickable]
 	if i == nil then
 		logmsg("Did not find clickable for "..clickable)
 		return
 	end
 	logmsg("Found clickable for "..clickable)
+
+	local pos = 0
+	for n, class in ipairs(i.class) do
+		if class == CLASS_LEV then
+			pos = n
+		end
+	end
+	if pos == 0 then
+		logmsg("No lever found")
+		return
+	end
+end
+
+-- pos: 1 (left click) or 2 (right click)
+-- action: BTN_UP, BTN_DOWN, BTN_SET
+-- clickable: Name of clickable
+-- value: Must be set if action == BTN_SET
+function click_clickable(pos, action, clickable, value)
+
+	if pos < 1 or pos > 2 then
+		logmsg("Only button 1 or 2 allowed, "..pos.." given")
+		return
+	end
+	local i = clickables[clickable]
+	if i == nil then
+		logmsg("Did not find clickable for "..clickable)
+		return
+	end
+	logmsg("Found clickable for "..clickable)
+
 	if i.class[pos] ~= CLASS_BTN and i.class[pos] ~= CLASS_TUMB then
 		logmsg("No button found in pos "..pos)
 		return
 	end
 	
+	-- Set value directly
+	if action == BTN_SET then
+		if not value then
+			logmsg("Attempting direct set of clickable without a value")
+			return
+		end
+		if value < i.arg_lim[pos][1] or value > i.arg_lim[pos][2] then
+			logmsg("Attempting to set value out of limits")
+			return
+		end
+		logmsg("Setting button to "..value)
+		GetDevice(i.device):performClickableAction(i.action[pos], value)
+		return
+	end
+
 	-- BTN down
-	if i.class[pos] == CLASS_BTN and action == 'D' then
+	if i.class[pos] == CLASS_BTN and action == BTN_DOWN then
 		logmsg("Setting button to "..i.arg_value[pos])
 		GetDevice(i.device):performClickableAction(i.action[pos], i.arg_value[pos])
 		return
 	end
 
 	-- BTN up
-	if action == 'U' then
+	if action == BTN_UP then
 		if i.class[pos] == CLASS_TUMB then
 			logmsg("Button up has no effect on tumbs")
 			return
@@ -256,20 +322,20 @@ function string_strip(s)
 	return s
 end
 
-function handle_subscribe(client, data)
+function handle_subscribe(client, cmd)
 	-- get:
 	-- * frequency
 	-- * precission
 	-- * own ID
-	local ownid
-	local frequency = 10
-	local cmd = string_split(data)
+	local opt_id
+	local opt_freq = 10
+	local opt_prec = 0
+
 	if cmd[2] == nil then
-		logmsg("No argument found")
+		logmsg("No gauge argument found")
 		return
 	end
 	local gauge_name = cmd[2]:lower()
-	local precission = tonumber(cmd[3])
 	local gauge = gauge_defs[gauge_name]
 	if gauge == nil then
 		logmsg("Found no gauge named "..gauge_name)
@@ -279,38 +345,60 @@ function handle_subscribe(client, data)
 		logmsg("No arg_number for arg "..gauge_name)
 		return
 	end
-	if ownid == nil then
-		ownid = gauge.arg_number
-	end
-	if precission == nil then
-		precission = 0
-	end
-	if frequency == 0 then
-		client.subscribed_gauges[gauge.arg_number] = nil
-		logmsg("Unsubscribing from "..gauge_name)
-		return
+	local options = cmd[3]
+	local opt_id = gauge.arg_number
+	if options then
+		if options.freq then
+			opt_freq = tonumber(options.freq)
+		end
+		if options.prec then
+			opt_prec = tonumber(options.prec)
+		end
+		if options.id then
+			opt_id = options.id
+		end
+		if opt_freq == 0 then
+			client.subscribed_gauges[gauge.arg_number] = nil
+			logmsg("Unsubscribing from "..gauge_name)
+			return
+		end
 	end
 	logmsg("Subscribing to "..gauge_name)
 	client.subscribed_gauges[gauge.arg_number] = { 
 		gauge = gauge,
-		precission = precission,
-		id = ownid,
-		period = 1/frequency
+		precission = opt_prec,
+		id = opt_id,
+		period = 1/opt_freq
 	}
 end
 
-function handle_button(data)
-	local pos = data:sub(2, 2)
-	local action = data:sub(3, 3)
-	local clickable = data:sub(5)
-	logmsg(string.format("class: %s, pos: %s, action: %s, clickable: %s",
-		class, pos, action, clickable))
-	pos = tonumber(pos)
-	if pos == nil then
-		logmsg(pos.." is not a number")
+function handle_button(client, cmd)
+	if #cmd < 4 then
+		logmsg("Not enough arguments to handle clickable")
 		return
 	end
-	click_clickable(pos, action, clickable)
+	local pos = cmd[2]
+	local action = cmd[3]
+	local clickable = cmd[4]:lower()
+	if type(pos) ~= "number" then
+		logmsg("Button number must be a number")
+		return
+	end
+	if type(action) ~= "number" then
+		logmsg("Action must be a number")
+		return
+	end
+	local value
+	if #cmd > 4 then
+		value = cmd[5]
+		if type(value) ~= "number" then
+			logmsg("Clickable value must be a number")
+			return
+		end
+	end
+	logmsg(string.format("pos: %d, action: %d, clickable: %s",
+		pos, action, clickable))
+	click_clickable(pos, action, clickable, value)
 end
 
 
@@ -321,35 +409,35 @@ function round(x, e)
 	return math.floor(x * d) / d
 end
 
-function send_json(client, send_values)
+function send_json(client, gauges)
 	local s = "[1"
-	for gauge, input in pairs(send_values) do
-		local fmt = ", [%s, %."..input[2].."f]"
-		s = s..fmt:format(gauge, input[1])
+	for _, gauge in pairs(gauges) do
+		local fmt = ", [%s, %."..gauge.precission.."f]"
+		s = s..fmt:format(gauge.id, gauge.gauge_value)
 	end
 	s = s.."]\n"
 	send_to_client(client, s)
 end
 
 function send_data(client)
-	local send_values = {}
-	local has_data = false
-	for arg_number, info in pairs(client.subscribed_gauges) do
-		local output = gauges:get_argument_value(arg_number)
-		input = round(interpolate(output, info.gauge.output, info.gauge.input), info.precission)
-		--local s = string.format("%s = %.3f", gauge, output)
-		--logmsg(s)	
-		if client.gauge_values[arg_number] == input then
+	local send_gauges = {}
+	local t = LoGetModelTime()
+	for arg_number, gauge in pairs(client.subscribed_gauges) do
+		if gauge.send_time and gauge.send_time + gauge.period >= t then
 			break
 		end
-		client.gauge_values[arg_number] = input
-		send_values[info.id] = { input, info.precission }
-		has_data = true
---		local s = string.format("%s = %.3f -> %.3f\n", gauge, output, input)
+		local output = gauges:get_argument_value(arg_number)
+		input = round(interpolate(output, gauge.gauge.output, gauge.gauge.input), gauge.precission)
+		if gauge.gauge_value == input then
+			break
+		end
+		gauge.gauge_value = input
+		gauge.send_time = t
+		send_gauges[#send_gauges+1] = gauge
 	end
-	if has_data then
+	if #send_gauges > 0 then
 		-- FIX: optional CBOR
-		send_json(client, send_values)
+		send_json(client, send_gauges)
 	end
 end
 
@@ -369,8 +457,7 @@ end
 function new_client(socket)
 	client = {
 		socket = socket,
-		subscribed_gauges = {},
-		gauge_values = {},
+		subscribed_gauges = {}
 	}
 	return client
 end
