16fcd09a3703684df53f392e6b8ee047c08fffd1
Bjorn Andersson <bjorn@iki.fi>
2019-01-08 00:04:42 +0200

    Remove support for clickables
    Clickable data is not needed, since everything can be done via
    other commands. This makes the implementation much less complex
    and it is easier to use too
    
    Some other restructuring and refactoring done as well.

--- server.lua
+++ server.lua
@@ -9,7 +9,7 @@ local logf 		-- log file
 
 local aircraft
 local gauges		-- in-game gauges from GetDevice(0)
-local gauge_defs	-- gauge definitions from JSON file
+local gauge_defs = {}	-- gauge definitions from JSON file
 -- gauge_defs maps gauge names and numbers to gauge definitions. The
 --		defintions are loaded from aircraft specifc JSON files.
 -- A gauge definition is a table with at least the following fields:
@@ -21,7 +21,8 @@ local gauge_defs	-- gauge definitions from JSON file
 --		to the input through interpolation. See the interpolate() function.
 -- * controller: Not used in this implementation
 
-local clickables	-- clickable definitions from JSON file
+local devices = {} -- device name to number mapping from JSON file
+local commands = {} -- command name to number mapping from JSON file
 local JSON
 local ready = false -- did we initialize properly?
 local clients = {} -- connected clients
@@ -34,17 +35,9 @@ local CLASS_LEV = 4
 local CLASS_MOVABLE_LEV = 5
 
 local CMD_AIRCRAFT = 0
-local CMD_BTN = 1 -- mouse click or scroll wheel
-local CMD_DEV = 2 -- device command
-local CMD_SUB = 3 -- subscribe to gauge value changes
+local CMD_DEV = 1 -- device command
+local CMD_SUB = 2 -- subscribe to gauge value changes
 
-
-local MOUSE_LEFT = 1
-local MOUSE_RIGHT = 2
-local MOUSE_SCROLL = 3
-local BTN_UP = 0   -- Release mouse button or scroll up
-local BTN_DOWN = 1 -- Click mouse button or scroll down
-local BTN_SET = 2  -- Direct set clickable or device command
 
 function LuaExportStart()
 	logf = io.open(lfs.writedir().."/Logs/dcs-master.log", "w")
@@ -62,12 +55,8 @@ function LuaExportStart()
 	server:settimeout(0)
 	gauges = GetDevice(0)
 	JSON = loadfile("Scripts/JSON.lua")()
-	if read_cockpit(self.Name) then
-		logmsg("Succesfully loaded cockpit info")
-		ready = true
-	else
-		logmsg("Failed to load cockpit info")
-	end
+	read_cockpit(self.Name)
+	ready = true
 end
 
 function LuaExportStop()
@@ -84,28 +73,30 @@ function LuaExportBeforeNextFrame()
 	for _, client in pairs(clients) do
 		local data = client.socket:receive()
 		if data then
-			logmsg("Received: "..data)
-			-- FIX: split on newline
-			-- FIX: check length
-			local cmd = JSON:decode(data)
-			if #cmd == 0 then
-				logmsg("Data contains no command")
+			-- workaround for missing 'continue' statement in Lua
+			while true do
+				logmsg("Received: "..data)
+				-- FIX: split on newline
+				-- FIX: check length
+				local cmd = JSON:decode(data)
+				if #cmd == 0 then
+					logmsg("Data contains no command")
+					break
+				end
+				local arg1 = cmd[1]
+				if type(arg1) ~= "number" then
+					logmsg("Command must be number")
+					break
+				end
+				if arg1 == CMD_DEV then
+					handle_device(client, cmd)
+				elseif arg1 == CMD_SUB then
+					handle_subscribe(client, cmd)
+				else
+					logmsg(string.format("Unknown command '%d'", arg1))
+				end
 				break
 			end
-			local arg1 = cmd[1]
-			if type(arg1) ~= "number" then
-				logmsg("Command must be number")
-				break
-			end
-			if arg1 == CMD_BTN then
-				handle_button(client, cmd)
-				break
-			end
-			if arg1 == CMD_SUB then
-				handle_subscribe(client, cmd)
-				break
-			end
-			logmsg(string.format("Unknown command '%d'", arg1))
 		end
 	end
 end
@@ -175,7 +166,8 @@ end
 function read_json(fn)
 	local jfile, err = io.open(fn, "rb")
 	if jfile == nil then
-		error(err)
+		logmsg(err)
+		return nil
 	end
 	local json_str = jfile:read("*a")
 	jfile:close()
@@ -185,63 +177,54 @@ end
 function load_gauges(aircraft)
 	local temp_gauges = read_json(lfs.writedir().."/Scripts/dcs-master/aircraft/"..aircraft.."/gauges.json")
 	if not temp_gauges then
-		logmsg("Failed to read gauges for "..aircraft)
-		return false
+		logmsg("No gauges for "..aircraft)
+		return
 	end
 	gauge_defs = {}
-	for k, v in pairs(temp_gauges) do
-		v.name = k
-		gauge_defs[k:lower()] = v
+	for i, v in ipairs(temp_gauges) do
+		-- index on both name and number
+		gauge_defs[v.name:lower()] = v
 		if v.arg_number then
 			gauge_defs[v.arg_number] = v
 		end
 	end
-	return true
+	return
 end
 
-
-function load_clickables(aircraft)
-	local temp_clickables = read_json(lfs.writedir().."/Scripts/dcs-master/aircraft/"..aircraft.."/clickables.json")
-	if not temp_clickables then
-		logmsg("Failed to read clickables for "..aircraft)
-		return false
-	end
-	clickables = {}
-	for k, v in pairs(temp_clickables) do
-		clickables[k:lower()] = v
-	end
-	return true
-end
-
-function read_cockpit(aircraft)
-	if not load_clickables(aircraft) then
+function load_devices(aircraft)
+	local temp_devices = read_json(lfs.writedir().."/Scripts/dcs-master/aircraft/"..aircraft.."/devices.json")
+	if not temp_devices then
+		logmsg("No device names for "..aircraft)
 		return
 	end
-	return load_gauges(aircraft) 
+	for k, v in pairs(temp_devices) do
+		devices[k:lower()] = v
+	end
 end
 
--- action: BTN_UP, BTN_DOWN, BTN_SET
--- clickable: Name of clickable
--- value: Must be set if action == BTN_SET
-function scroll_clickable(action, clickable, value)
-
-	local i = clickables[clickable]
-	if i == nil then
-		logmsg("Did not find clickable for "..clickable)
+function load_commands(aircraft)
+	local temp_commands = read_json(lfs.writedir().."/Scripts/dcs-master/aircraft/"..aircraft.."/commands.json")
+	if not temp_commands then
+		logmsg("No command names for "..aircraft)
 		return
 	end
-	logmsg("Found clickable for "..clickable)
-
-	local pos = 0
-	for n, class in ipairs(i.class) do
-		if class == CLASS_LEV then
-			pos = n
+	for k, v in pairs(temp_commands) do
+		local command = k:lower()
+		commands[command] = v
+		-- If there is a '.' in the name then also index on the
+		-- part after that alone
+		local dot = command:find(".", 1, true)
+		if dot then
+			commands[command:sub(dot+1)] = v
 		end
 	end
-	if pos == 0 then
-		logmsg("No lever found")
-		return
-	end
+end
+	
+
+function read_cockpit(aircraft)
+	load_gauges(aircraft) 
+	load_devices(aircraft)
+	load_commands(aircraft)
 end
 
 
@@ -256,87 +239,40 @@ function round(x, n)
     return x / n
 end
 
--- pos: MOUSE_LEFT or MOUSE_RIGHT
--- action: BTN_UP, BTN_DOWN, BTN_SET
--- clickable: Name of clickable
--- value: Must be set if action == BTN_SET
-function click_clickable(pos, action, clickable, value)
 
-	if pos ~= MOUSE_LEFT and pos ~= MOUSE_RIGHT then
-		logmsg("Only button 1 or 2 allowed, "..pos.." given")
+-- cmd: [2, device, command, value ]
+function handle_device(client, cmd)
+	if #cmd ~= 4 then
+		logmsg("Not enough values for command")
 		return
 	end
-	local i = clickables[clickable]
-	if i == nil then
-		logmsg("Did not find clickable for "..clickable)
-		return
-	end
-	logmsg("Found clickable for "..clickable)
-
-	if i.class[pos] ~= CLASS_BTN and i.class[pos] ~= CLASS_TUMB then
-		logmsg("No button found in pos "..pos)
-		return
-	end
-	
-	-- Set value directly
-	if action == BTN_SET then
-		if not value then
-			logmsg("Attempting direct set of clickable without a value")
+	local device = tonumber(cmd[2])
+	if device == nil then
+		device = devices[cmd[2]:lower()]
+		if device == nil then
+			logmsg("Found no device named "..cmd[2])
 			return
 		end
-		if value < i.arg_lim[pos][1] or value > i.arg_lim[pos][2] then
-			logmsg("Attempting to set value out of limits")
+	end
+	local command = tonumber(cmd[3])
+	if command == nil then
+		command = commands[cmd[3]:lower()]
+		if command == nil then
+			logmsg("Found no command named "..cmd[3])
 			return
 		end
-		logmsg("Setting button to "..value)
-		GetDevice(i.device):performClickableAction(i.action[pos], value)
+	end
+	local value = tonumber(cmd[4])
+	if value == nil then
+		logmsg("Value must be a number")
+		return
+	end
+	if value < -1.0 or value > 1.0 then
+		logmsg("Value must be withing the range [-1.0 1.0] inclusive")
 		return
 	end
 
-	-- BTN down
-	if i.class[pos] == CLASS_BTN and action == BTN_DOWN then
-		logmsg("Setting button to "..i.arg_value[pos])
-		GetDevice(i.device):performClickableAction(i.action[pos], i.arg_value[pos])
-		return
-	end
-
-	-- BTN up
-	if action == BTN_UP then
-		if i.class[pos] == CLASS_TUMB then
-			logmsg("Button up has no effect on tumbs")
-			return
-		end
-		if i.stop_action[pos] == nil then
-			logmsg("No stop_action found for button")
-			return
-		end
-		GetDevice(i.device):performClickableAction(i.stop_action[pos], 0)
-		return
-	end
-		
-	-- TUMB down
-	logmsg(string.format("device: %d, action: %d, arg_value: %d", i.device, i.action[pos], i.arg_value[pos]))
-	local v = gauges:get_argument_value(i.arg[pos])
-
-	local newval = v + i.arg_value[pos]
-	-- Fix rounding errors in floats
-	-- Maybe we should round v instead?
-	newval = round(newval, 3)
-	logmsg("current: "..v..", adding: "..i.arg_value[pos]..", newval: "..newval)
-	if i.cycle == false and (newval < i.arg_lim[pos][1] or newval > i.arg_lim[pos][2]) then
-		logmsg("New val outside limits")
-		return
-	end
-	if newval < i.arg_lim[pos][1] then
-		logmsg("Cycling value")
-		newval = i.arg_lim[pos][2]
-	elseif newval > i.arg_lim[pos][2] then
-		logmsg("Cycling value")
-		newval = i.arg_lim[pos][1]
-	end	
-	logmsg("Setting new value "..newval)
-	GetDevice(i.device):performClickableAction(i.action[pos], newval)
-	logmsg("Done")
+	GetDevice(device):performClickableAction(command, value)
 end
 
 
@@ -401,41 +337,6 @@ function handle_subscribe(client, cmd)
 	}
 end
 
--- cmd: [ 1, pos, action, clickable, value ]
---   where:
---   - pos: 1 = left button, 2 = right button
---   - action: 0/BTN_UP, 1/BTN_DOWN, 2/BTN_SET
---   - clickable: name of clickable
---   - value: for BTN_SET
-
-function handle_button(client, cmd)
-	if #cmd < 4 then
-		logmsg("Not enough arguments to handle clickable")
-		return
-	end
-	local pos = cmd[2]
-	local action = cmd[3]
-	local clickable = cmd[4]:lower()
-	if type(pos) ~= "number" then
-		logmsg("Button number must be a number")
-		return
-	end
-	if type(action) ~= "number" then
-		logmsg("Action must be a number")
-		return
-	end
-	local value
-	if #cmd > 4 then
-		value = cmd[5]
-		if type(value) ~= "number" then
-			logmsg("Clickable value must be a number")
-			return
-		end
-	end
-	logmsg(string.format("pos: %d, action: %d, clickable: %s",
-		pos, action, clickable))
-	click_clickable(pos, action, clickable, value)
-end
 
 function send_json(client, gauges)
 	local s = "["..CMD_SUB
